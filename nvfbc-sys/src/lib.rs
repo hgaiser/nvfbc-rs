d1aca7429368672b80c77c94c6c6217ee5ccc753
Maarten de Vries <maarten@de-vri.es>
2022-06-23 14:39:57 +0200

    Commit the generated code instead of running bindgen at build time.

--- nvfbc-sys/src/lib.rs
+++ nvfbc-sys/src/lib.rs
@@ -1,7 +1,9 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+mod generated;
+pub use generated::*;
 
 pub const NVFBC_VERSION: u32 = NVFBC_VERSION_MINOR | (NVFBC_VERSION_MAJOR << 8);
 
